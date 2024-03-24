5bccf3e031fda2b8b8efdeb47e64c5cf3542e15b
edizan <161873395+edizan@users.noreply.github.com>
2024-03-24 10:59:45 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,25 +1,41 @@
 import discord
 
-# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+from discord.ext import commands
+
+ 
+
 intents = discord.Intents.default()
-# Mesajları okuma ayrıcalığını etkinleştirelim
+
 intents.message_content = True
-# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
-client = discord.Client(intents=intents)
 
-@client.event
+ 
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+ 
+
+@bot.event
+
 async def on_ready():
-    print(f'{client.user} olarak giriş yaptık.')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$merhaba'):
-        await message.channel.send("Selam!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    else:
-        await message.channel.send(message.content)
+    print(f'{bot.user} olarak giriş yaptık')
 
-client.run("BOTUNUZUN TOKEN BURADA OLMALIDIR!")
+ 
+
+@bot.command()
+
+async def hello(ctx):
+
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+ 
+
+@bot.command()
+
+async def heh(ctx, count_heh = 5):
+
+    await ctx.send("he" * count_heh)
+
+ 
+
+bot.run("a3366c2e000812a22b8e83c919f202e5e851126e85d0d16c27bc89a9afac8cfa")
